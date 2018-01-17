645dd91f22b6e155c8861d104bf1676b394195aa
Алексей <C00Lman041193@gmail.com>
2018-01-17 11:21:24 +0300

    diffuse and specular maps

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ Camera camera(glm::vec3(0.f, 0.f, 3.f));
 void processInput(GLFWwindow *window, float dt);
 void mouse_callback(GLFWwindow *window, double x, double y);
 void scroll_callback(GLFWwindow *window, double dx, double dy);
+GLuint loadTexture(const char *path);
 
 void error_callback(int error, const char* description)
 {
@@ -94,66 +95,86 @@ int main(int argc, char ** argv)
 	Shader lampShader("shaders/vertex_lamp.glsl", "shaders/fragment_lamp.glsl");
 
 	GLfloat vertices[] = {
-		-1, 1.618, 0, 	0.f, 3.236f, 1.236f,	1.f,
-		1, 1.618, 0, 	0.f, 3.236f, 1.236f,	1.f,
-		0, 1, 1.618, 	0.f, 3.236f, 1.236f,	1.f,//abc
-		-1, 1.618, 0, 	0.f, 3.236f, -1.236f,	2.f,
-		1, 1.618, 0, 	0.f, 3.236f, -1.236f,	2.f,
-		0, 1, -1.618, 	0.f, 3.236f, -1.236f,	2.f, //abg
-		1, 1.618, 0, 	2.f, 2.f, 2.f,			3.f,
-		0, 1, 1.618, 	2.f, 2.f, 2.f,			3.f,
-		1.618, 0, 1, 	2.f, 2.f, 2.f,			3.f, //bce
-		1, 1.618, 0, 	3.236f, 1.236f, 0.f,	4.f,
-		1.618, 0, 1, 	3.236f, 1.236f, 0.f,	4.f,
-		1.618, 0, -1, 	3.236f, 1.236f, 0.f,	4.f, //bef
-		1, 1.618, 0, 	2.f, 2.f, -2.f,			5.f,
-		1.618, 0, -1, 	2.f, 2.f, -2.f,			5.f,
-		0, 1, -1.618, 	2.f, 2.f, -2.f,			5.f, //bfg
-		-1, 1.618, 0, 	-2.f, 2.f, -2.f,		6.f,
-		0, 1, -1.618, 	-2.f, 2.f, -2.f,		6.f,
-		-1.618, 0, -1, 	-2.f, 2.f, -2.f,		6.f, //agl
-		-1, 1.618, 0, 	-3.236f, 1.236f, 0.f,	7.f,
-		-1.618, 0, 1, 	-3.236f, 1.236f, 0.f,	7.f,
-		-1.618, 0, -1, 	-3.236f, 1.236f, 0.f,	7.f, //akl
-		-1, 1.618, 0, 	-2.f, 2.f, 2.f,			8.f,
-		-1.618, 0, 1, 	-2.f, 2.f, 2.f,			8.f,
-		0, 1, 1.618, 	-2.f, 2.f, 2.f,			8.f, //akc
-		0, 1, 1.618, 	-1.236f, 0.f, 3.236f,	9.f,
-		-1.618, 0, 1, 	-1.236f, 0.f, 3.236f,	9.f,
-		0, -1, 1.618, 	-1.236f, 0.f, 3.236f,	9.f, //ckd
-		0, 1, 1.618, 	1.236f, 0.f, 3.236f,	10.f,
-		0, -1, 1.618, 	1.236f, 0.f, 3.236f,	10.f,
-		1.618, 0, 1, 	1.236f, 0.f, 3.236f,	10.f, //cde
-		0, 1, -1.618, 	-1.236f, 0.f, -3.236f,	11.f,
-		-1.618, 0, -1, 	-1.236f, 0.f, -3.236f,	11.f,
-		0, -1, -1.618, 	-1.236f, 0.f, -3.236f,	11.f, //glh
-		0, 1, -1.618, 	1.236f, 0.f, -3.236f,	12.f,
-		0, -1, -1.618, 	1.236f, 0.f, -3.236f,	12.f,
-		1.618, 0, -1,	1.236f, 0.f, -3.236f,	12.f, //ghf
-		0, -1, 1.618, 	-2.f, -2.f, 2.f,		13.f,
-		-1.618, 0, 1, 	-2.f, -2.f, 2.f,		13.f,
-		-1, -1.618, 0, 	-2.f, -2.f, 2.f,		13.f, //dkj
-		-1, -1.618, 0, 	-3.236f, -1.236f, 0.f,	14.f,
-		-1.618, 0, 1, 	-3.236f, -1.236f, 0.f,	14.f,
-		-1.618, 0, -1, 	-3.236f, -1.236f, 0.f,	14.f, //jkl
-		-1, -1.618, 0, 	-2.f, -2.f, -2.f,		15.f,
-		-1.618, 0, -1, 	-2.f, -2.f, -2.f,		15.f,
-		0, -1, -1.618, 	-2.f, -2.f, -2.f,		15.f, //jlh
-		0, -1, 1.618, 	0.f, -3.236f, 1.236f,	16.f,
-		-1, -1.618, 0, 	0.f, -3.236f, 1.236f,	16.f,
-		1, -1.618, 0, 	0.f, -3.236f, 1.236f,	16.f, //dji
-		-1, -1.618, 0, 	0.f, -3.236f, -1.236f,	17.f,
-		1, -1.618, 0, 	0.f, -3.236f, -1.236f,	17.f,
-		0, -1, -1.618, 	0.f, -3.236f, -1.236f,	17.f, //jih
-		0, -1, 1.618, 	2.f, -2.f, 2.f,			18.f,
-		1.618, 0, 1, 	2.f, -2.f, 2.f,			18.f,
-		1, -1.618, 0, 	2.f, -2.f, 2.f,			18.f, //dei
-		1.618, 0, 1, 	3.236f, -1.236f, 0.f,	19.f,
-		1, -1.618, 0, 	3.236f, -1.236f, 0.f,	19.f,
-		1.618, 0, -1, 	3.236f, -1.236f, 0.f,	19.f, //eif
-		1, -1.618, 0, 	2.f, -2.f, -2.f,		20.f,
-		1.618, 0, -1, 	2.f, -2.f, -2.f,		20.f,
-		0, -1, -1.618, 	2.f, -2.f, -2.f,		20.f //ifh
+		//abc
+		-1, 1.618, 0, 	0.f, 3.236f, 1.236f,	0.f, 0.f,
+		1, 1.618, 0, 	0.f, 3.236f, 1.236f,	0.f, 1.f,
+		0, 1, 1.618, 	0.f, 3.236f, 1.236f,	1.f, 0.f,
+		//abg
+		-1, 1.618, 0, 	0.f, 3.236f, -1.236f,	0.f, 0.f,
+		1, 1.618, 0, 	0.f, 3.236f, -1.236f,	0.f, 1.f,
+		0, 1, -1.618, 	0.f, 3.236f, -1.236f,	1.f, 0.f,
+		//bce
+		1, 1.618, 0, 	2.f, 2.f, 2.f,			0.f, 0.f,
+		0, 1, 1.618, 	2.f, 2.f, 2.f,			0.f, 1.f,
+		1.618, 0, 1, 	2.f, 2.f, 2.f,			1.f, 0.f,
+		//bef
+		1, 1.618, 0, 	3.236f, 1.236f, 0.f,	0.f, 0.f,
+		1.618, 0, 1, 	3.236f, 1.236f, 0.f,	0.f, 1.f,
+		1.618, 0, -1, 	3.236f, 1.236f, 0.f,	1.f, 0.f,
+		//bfg
+		1, 1.618, 0, 	2.f, 2.f, -2.f,			0.f, 0.f,
+		1.618, 0, -1, 	2.f, 2.f, -2.f,			0.f, 1.f,
+		0, 1, -1.618, 	2.f, 2.f, -2.f,			1.f, 0.f,
+		//agl
+		-1, 1.618, 0, 	-2.f, 2.f, -2.f,		0.f, 0.f,
+		0, 1, -1.618, 	-2.f, 2.f, -2.f,		0.f, 1.f,
+		-1.618, 0, -1, 	-2.f, 2.f, -2.f,		1.f, 0.f,
+		//akl
+		-1, 1.618, 0, 	-3.236f, 1.236f, 0.f,	0.f, 0.f,
+		-1.618, 0, 1, 	-3.236f, 1.236f, 0.f,	0.f, 1.f,
+		-1.618, 0, -1, 	-3.236f, 1.236f, 0.f,	1.f, 0.f,
+		//akc
+		-1, 1.618, 0, 	-2.f, 2.f, 2.f,			0.f, 0.f,
+		-1.618, 0, 1, 	-2.f, 2.f, 2.f,			0.f, 1.f,
+		0, 1, 1.618, 	-2.f, 2.f, 2.f,			1.f, 0.f,
+		//ckd
+		0, 1, 1.618, 	-1.236f, 0.f, 3.236f,	0.f, 0.f,
+		-1.618, 0, 1, 	-1.236f, 0.f, 3.236f,	0.f, 1.f,
+		0, -1, 1.618, 	-1.236f, 0.f, 3.236f,	1.f, 0.f,
+		//cde
+		0, 1, 1.618, 	1.236f, 0.f, 3.236f,	0.f, 0.f,
+		0, -1, 1.618, 	1.236f, 0.f, 3.236f,	0.f, 1.f,
+		1.618, 0, 1, 	1.236f, 0.f, 3.236f,	1.f, 0.f,
+		//glh
+		0, 1, -1.618, 	-1.236f, 0.f, -3.236f,	0.f, 0.f,
+		-1.618, 0, -1, 	-1.236f, 0.f, -3.236f,	0.f, 1.f,
+		0, -1, -1.618, 	-1.236f, 0.f, -3.236f,	1.f, 0.f,
+		//ghf
+		0, 1, -1.618, 	1.236f, 0.f, -3.236f,	0.f, 0.f,
+		0, -1, -1.618, 	1.236f, 0.f, -3.236f,	0.f, 1.f,
+		1.618, 0, -1,	1.236f, 0.f, -3.236f,	1.f, 0.f,
+		//dkj
+		0, -1, 1.618, 	-2.f, -2.f, 2.f,		0.f, 0.f,
+		-1.618, 0, 1, 	-2.f, -2.f, 2.f,		0.f, 1.f,
+		-1, -1.618, 0, 	-2.f, -2.f, 2.f,		1.f, 0.f,
+		//jkl
+		-1, -1.618, 0, 	-3.236f, -1.236f, 0.f,	0.f, 0.f,
+		-1.618, 0, 1, 	-3.236f, -1.236f, 0.f,	0.f, 1.f,
+		-1.618, 0, -1, 	-3.236f, -1.236f, 0.f,	1.f, 0.f,
+		//jlh
+		-1, -1.618, 0, 	-2.f, -2.f, -2.f,		0.f, 0.f,
+		-1.618, 0, -1, 	-2.f, -2.f, -2.f,		0.f, 1.f,
+		0, -1, -1.618, 	-2.f, -2.f, -2.f,		1.f, 0.f,
+		//dji
+		0, -1, 1.618, 	0.f, -3.236f, 1.236f,	0.f, 0.f,
+		-1, -1.618, 0, 	0.f, -3.236f, 1.236f,	0.f, 1.f,
+		1, -1.618, 0, 	0.f, -3.236f, 1.236f,	1.f, 0.f,
+		//jih
+		-1, -1.618, 0, 	0.f, -3.236f, -1.236f,	0.f, 0.f,
+		1, -1.618, 0, 	0.f, -3.236f, -1.236f,	0.f, 1.f,
+		0, -1, -1.618, 	0.f, -3.236f, -1.236f,	1.f, 0.f,
+		//dei
+		0, -1, 1.618, 	2.f, -2.f, 2.f,			0.f, 0.f,
+		1.618, 0, 1, 	2.f, -2.f, 2.f,			0.f, 1.f,
+		1, -1.618, 0, 	2.f, -2.f, 2.f,			1.f, 0.f,
+		//eif
+		1.618, 0, 1, 	3.236f, -1.236f, 0.f,	0.f, 0.f,
+		1, -1.618, 0, 	3.236f, -1.236f, 0.f,	0.f, 1.f,
+		1.618, 0, -1, 	3.236f, -1.236f, 0.f,	1.f, 0.f,
+		//ifh
+		1, -1.618, 0, 	2.f, -2.f, -2.f,		0.f, 0.f,
+		1.618, 0, -1, 	2.f, -2.f, -2.f,		0.f, 1.f,
+		0, -1, -1.618, 	2.f, -2.f, -2.f,		1.f, 0.f,
 	};
 	GLuint VBO, VAO;
 	glGenVertexArrays(1, &VAO);
@@ -163,18 +184,21 @@ int main(int argc, char ** argv)
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (GLvoid *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)0);
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (GLvoid *)(6 * sizeof(GLfloat)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)(6 * sizeof(GLfloat)));
 
 	GLuint lightVAO;
 	glGenVertexArrays(1, &lightVAO);
 	glBindVertexArray(lightVAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (GLvoid *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *)0);
 	glEnableVertexAttribArray(0);
+
+	GLuint diffuseMap = loadTexture("textures/container2.png");
+	GLuint specularMap = loadTexture("textures/container2_specular.png");
 
 	float dt = 0.f;
 	float lastFrame = 0.f;
@@ -188,12 +212,11 @@ int main(int argc, char ** argv)
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		shader.use();
-		shader.setVec3("material.ambient", 0.25f, 0.25f, 0.25f);
-		shader.setVec3("material.diffuse", 0.4f, 0.4f, 0.4f);
-		shader.setVec3("material.specular", 0.774597f, 0.774597f, 0.774597f);
+		shader.setInt("material.diffuse", 0);
+		shader.setInt("material.specular", 1);
 		shader.setFloat("material.shininess", 32.f);
-		shader.setVec3("light.ambient", 1.0f, 1.0f, 1.0f);
-		shader.setVec3("light.diffuse", 1.0f, 1.0f, 1.0f);
+		shader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
+		shader.setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
 		shader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 		shader.setVec3("light.position", lightPos.x, lightPos.y, lightPos.z);
 		shader.setVec3("viewPos", camera.Position.x, camera.Position.y, camera.Position.z);
@@ -207,7 +230,12 @@ int main(int argc, char ** argv)
 		model = glm::translate(model, objectPos);
 		model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
 		shader.setMat4("model", model);
-		
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, diffuseMap);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, specularMap);
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 60);
 
@@ -279,4 +307,41 @@ void mouse_callback(GLFWwindow *window, double x, double y)
 void scroll_callback(GLFWwindow *window, double dx, double dy)
 {
 	camera.ProcessMouseScroll(dy);
+}
+
+GLuint loadTexture(const char *path)
+{
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+
+	int width, height, nrComponents;
+	unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+	}
+	else
+	{
+		std::cout << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+	}
+
+	return textureID;
 }
